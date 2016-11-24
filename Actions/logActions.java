cd32bccf84ac212e197b99cb0aaf4fbf8849c4fd
arielrojasintertect <ariel.rojas@intertecintl.com>
2016-11-23 21:52:20 -0600

    commit

--- Actions/logActions.java
+++ Actions/logActions.java
@@ -13,11 +13,13 @@ import objectRepository.MicrosoftLoginPage;
 public class logActions{
 	objectRepository.MicrosoftLoginPage login;
 	WebDriver driver;
+	
 	public logActions(WebDriver driver) {
 		this.driver=driver;
 		login = new MicrosoftLoginPage();
 		
 	}
+
 	public void login() throws Exception{
 		//this pass and user should be set in a xml with usernames and passwords
 		driver.findElement(login.username).sendKeys("ariel.rojas@intertecintl.com");
